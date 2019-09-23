b2fa92b6e325746b6dd4efb325633121fc70a431
Junya Kemmochi <github@tsurugeek.com>
2019-09-23 16:08:13 +0900

    change class component to functional component.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    const greeting = "Hi!"
-    const dom = <h1>{greeting}</h1>
-    return (
-      <React.Fragment>
-        <h1>Hi</h1>
-        <label htmlFor="aa">this is label</label>
-        <input type="text" className="aaa" onClick={() => {console.log("I am clicked")}} />
-      </React.Fragment>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     const greeting = "Hi!"
+//     const dom = <h1>{greeting}</h1>
+//     return (
+//       <React.Fragment>
+//         <h1>Hi</h1>
+//         <label htmlFor="aa">this is label</label>
+//         <input type="text" className="aaa" onClick={() => {console.log("I am clicked")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </React.Fragment>
+  )
+}
+
+const Cat = () => {
+  return <div>meow</div>
 }
 
 export default App;
