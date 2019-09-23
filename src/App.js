a35ce5e72a508338ad1e35be7be4a46612a9562a
Junya Kemmochi <github@tsurugeek.com>
2019-09-23 12:12:44 +0900

    change jsx code to use several features.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello, world</h1>
+    const greeting = "Hi!"
+    const dom = <h1>{greeting}</h1>
+    return (
+      <React.Fragment>
+        <h1>Hi</h1>
+        <label htmlFor="aa">this is label</label>
+        <input type="text" className="aaa" onClick={() => {console.log("I am clicked")}} />
+      </React.Fragment>
+    )
   }
 }
 
