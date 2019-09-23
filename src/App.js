55537044e7e386d219441e7d9e40aa97d9c92540
Junya Kemmochi <github@tsurugeek.com>
2019-09-23 11:49:24 +0900

    change function to class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello, world</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <h1>Hello, world</h1>
+  }
 }
 
 export default App;
