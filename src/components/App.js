50a66b40750b5327d57f04c61197022938281971
Junya Kemmochi <github@tsurugeek.com>
2019-09-28 10:52:44 +0900

    implement App with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { increment, decrement } from '../actions'
 
-const App = () => ( <Counter /> )
-
-class Counter extends Component {
-  constructor(props){
-    super(props)
-    this.state = {count: 0}
-  }
-
-  // handlePlusButton() {
-  //   console.log(this)
-  //   this.setState({count: this.state.count + 1})
-  // }
-  handlePlusButton = () => {
-    this.setState({count: this.state.count + 1})
-  }
-
-  handleMinusButton = () => {
-    this.setState({count: this.state.count - 1})
-  }
-
+class App extends Component {
   render() {
+    const props = this.props
     return (
       <React.Fragment>
-        <div>{this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>count: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log('mapStateToProps')
+  console.log(state)
+  return { value: state.count.value }
+}
+// アクションに対応するreducerを実行する関数を返しているイメージかな
+// const mapDispatchToProps = dispatch => {
+//   console.log('mapDispatchToProps')
+//   return {
+//     increment: () => {
+//       console.log('increment')
+//       dispatch(increment())
+//     },
+//     decrement: () => {
+//       console.log('decrement')
+//       dispatch(decrement())
+//     }
+//   }
+// }
+
+// アクションを返しているイメージだな。こっちの方が直感的にわかりやすい。
+// つまり、connectの引数がstateとactionになっているから。
+const mapDispatchToProps = { increment, decrement}
+
+// Appのラッパーを作ってstateとactionメソッドをAppにpropsとして渡している感じかな。
+export default connect(mapStateToProps, mapDispatchToProps)(App)
